dfbb1355ff0f1ab991fe8eb59dbb0acb684aecda
Fawad Ahmad <fawadahmad0059@gmail.com>
2025-06-13 10:43:00 +0500

    Added a hamburger menu

---
+++ hammenu.js
@@ -0,0 +1,10 @@
+
+
+
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('nav ul');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamburger.classList.toggle('active');
+  });
